97f9eef32dd6777653229195dc89ec120e1f9ec5
Zhong Zhuang <naituida@foxmail.com>
2022-08-21 16:45:50 +0800

    add Hint Finance

---
+++ script/ElectricSheep.s.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.8.16;
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import "../src/ElectricSheep/contracts/Setup.sol";
+import "../src/MerkleDrop/contracts/Setup.sol";
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract ElectricSheepScript is Script, Test {
+    Setup setup;
+    ERC20 dreamer;
+
+    function setUp() public {
+        setup = new Setup();
+        dreamer = setup.DREAMERS();
+        vm.createSelectFork("eth");
+    }
+
+    function run() public {
+
+    }
+}
